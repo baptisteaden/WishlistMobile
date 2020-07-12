acfd3548bb9badbf2c526e0cdca7bff858a270eb
kigamii <baptiste.aden@gmail.com>
2020-07-12 21:50:45 +0200

    ✨NEW: Heroku integration for server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const expressJWT = require('express-jwt');
 const userRouter = require('./routes/user');
@@ -17,10 +16,11 @@ app.use(
   expressJWT({
     secret: process.env.JWT_SECRET,
     algorithms: ['HS256'],
-  }).unless({ path: '/user' }),
+  }).unless({ path: ['/', '/user'] }),
 );
 
 //routes
+app.get('/', (req, res) => res.json({ Hey: 'Welcome to my wishlist API' }));
 app.use('/user', userRouter);
 app.use('/wish', wishRouter);
 app.use('/friend', friendRouter);
@@ -30,4 +30,6 @@ app.use((req, res) => {
   res.status(404).send('<h1>404 Not Found</h1>');
 });
 
-module.exports.handler = serverless(app);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port ', process.env.PORT || 3000);
+});
